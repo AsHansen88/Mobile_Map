760d3bc6ffe735e5e5bd15b73ff1cfddb5c20902
bobby <ande970s@stud.kea.dk>
2023-10-02 14:23:06 +0200

    :)

--- App.js
+++ App.js
@@ -1,10 +1,88 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { useEffect, useRef, useState } from 'react';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [markers, setMarkers] = useState([]);
+  const [region, setRegion] = useState({
+    latitude: 55, 
+    longitude: 12, 
+    latitudeDelta: 20,
+    longitudeDelta: 20,
+  });
+
+  const MapViewRef = useRef(null);
+  const locationSubscription = useRef(null);
+
+  useEffect(() => {
+    async function startListening() {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        alert("No access to location");
+      } else {
+        locationSubscription.current = await Location.watchPositionAsync(
+          {
+            distanceInterval: 100,
+            accuracy: Location.Accuracy.High,
+          },
+          (location) => {
+            const newRegion = {
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: 0.028,
+              longitudeDelta: 0.02,
+            };
+            setRegion(newRegion);
+            if (MapViewRef.current) {
+              MapViewRef.current.animateToRegion(newRegion);
+            }
+          }
+        );
+      }
+    }
+
+    startListening();
+
+    return () => {
+      if (locationSubscription.current) {
+        locationSubscription.current.remove();
+      }
+    };
+  }, []);
+
+  function addMarker(data) {
+    const { latitude, longitude } = data.nativeEvent.coordinate;
+    const newMarker = {
+      coordinate: { latitude, longitude },
+      key: data.timeStamp.toString(),
+      title: "Great place",
+    };
+    setMarkers([...markers, newMarker]);
+  }
+
+  function onMarkerPressed(text) {
+    alert("You pressed " + text);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView
+        style={styles.map}
+        region={region}
+        onLongPress={addMarker}
+        ref={MapViewRef}
+      >
+        {markers.map((marker) => (
+          <Marker
+            coordinate={marker.coordinate}
+            key={marker.key}
+            title={marker.title}
+            onPress={() => onMarkerPressed(marker.title)}
+          />
+        ))}
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +91,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    flex: 1,
   },
 });
+
